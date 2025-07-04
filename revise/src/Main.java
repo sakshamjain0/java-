99d3c671738cde607f8dc54555da175887219227
sakshamjain0 <jsaksham1473@gmail.com>
2025-07-04 15:21:04 +0530

    Add files via upload

---
+++ revise/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(string[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt;
+        System.out.print(i);
+    }
+}
